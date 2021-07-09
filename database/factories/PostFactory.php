ce72c63401b470f2912f784d2aa103071981f796
noname <you@example.com>
2021-07-09 21:05:25 +0800

    post model and seeder

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -6,7 +6,13 @@ use App\Models\Post;
 use Faker\Generator as Faker;
 
 $factory->define(Post::class, function (Faker $faker) {
+    $date_time = $faker->date . ' ' . $faker->time;
+    $content = implode('<br />', $faker->paragraphs(3));
     return [
-        //
+        // 'user_id' =>,
+        'title'      => $faker->sentence(),
+        'content'    => $content,
+        'created_at' => $date_time,
+        'updated_at' => $date_time,
     ];
 });
--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class PostsTableSeeder extends Seeder
@@ -11,6 +12,12 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user_ids = ['1', '2', '3', '4', '5', '6', '7', '8'];
+        $faker = app(Faker\Generator::class);
+
+        $posts = factory(Post::class)->times(100)->make()->each(function ($post) use ($faker, $user_ids) {
+            $post->user_id = $faker->randomElement($user_ids);
+        });
+        Post::insert($posts->toArray());
     }
 }
