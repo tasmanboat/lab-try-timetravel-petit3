081e9028823a7b43fdaab7c25800cb8ea15190a9
noname <you@example.com>
2021-07-09 21:40:31 +0800

    owner_only

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -10,9 +10,22 @@
   </div>
   <div class="card-text">{!! $post->content !!}</div>
 </div>
-<div class="card-body">
+{{-- <div class="card-body">
   <a href="{{ route('posts.show', $post) }}">Permanent link</a>
   | <a href="{{ route('posts.edit', $post) }}">Edit</a> |
   <a href="#">Delete</a>
+</div> --}}
+<div class="card-body">
+  <a href="{{ route('posts.show', $post) }}">Permanent link</a>
+  @can('update', $post->user)
+  | <a href="{{ route('posts.edit', $post) }}">Edit</a> |
+  <form action="{{ route('posts.destroy', $post->id) }}" method="POST"
+    onsubmit="return confirm('Confirm: delete Post #{{ $post->id }} ?');"
+    style="display:inline !important;">
+    {{ csrf_field() }}
+    {{ method_field('DELETE') }}
+    <button type="submit" class="btn btn-link" style="padding:0 !important;">Delete</button>
+  </form>
+  @endcan
 </div>
 @endsection
