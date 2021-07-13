95f6aa4f39335b75eb63441d58731f654bd4f887
tasmanboat <74056379+tasmanboat@users.noreply.github.com>
2021-07-13 23:32:58 +0800

    Update show.blade.php

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -5,8 +5,8 @@
 <div class="card-body">
   <div class="card-title"><h4>User #{{ $user->id }}</h4></div>
   <div class="card-text">
-      <p class="font-weight-bold">{{ $user->name }}</p>
-      <p>{{ $user->email }}</p>
+    <p class="font-weight-bold">{{ $user->name }}</p>
+    <p>{{ $user->email }}</p>
   </div>
 </div>
 
